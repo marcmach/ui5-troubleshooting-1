971668164aaec3852cf9f68481906ca28266697d
Marcelo <marceloma56@gmail.com>
2021-02-06 21:26:36 +0000

    corrigido

--- ex7/src/logica.controller.js
+++ ex7/src/logica.controller.js
@@ -2,12 +2,12 @@ sap.ui.controller("ovly.logica", {
 	onInit: function () {
 		var sInputId = this.createId("input_idioma");
 		var oInput = sap.ui.getCore().byId(sInputId);
-		oInput.setText(sap.ui.getCore().getConfiguration().getLanguage());
+		oInput.setValue(sap.ui.getCore().getConfiguration().getLanguage());
 	},
 
 	setRtl: function (oControlEvent) {
 		var oParameters = oControlEvent.getParameters();
-		var bSelected = oParameters;
+		var bSelected = oParameters.selected;
 		sap.ui.getCore().getConfiguration().setRTL(bSelected);
 	},
 	
@@ -17,7 +17,7 @@ sap.ui.controller("ovly.logica", {
 		* Vide documentação dos eventos da classe BarcodeScannerButton
 		* e usar o evento acionado ao escanear com sucesso um código de barra
 		*/
-		var sBarcode = "XXX"; 
+		var sBarcode = oDadosDoEvento.getParameters().text; 
 		alert(sBarcode);
 	}
 });
